e8381f415b26eb9f180330834e03e7ca8bb34709
Alexander Navarro <ale.navarro.parra@gmail.com>
2023-05-16 17:21:42 -0400

    update which key

---
+++ config/nvim/lua/aleidk/plugins/which-key.lua
@@ -0,0 +1,26 @@
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		plugins = { spelling = true },
+		defaults = {
+			mode = { "n", "v" },
+			["g"] = { name = "+goto" },
+			["gz"] = { name = "+surround" },
+			["<leader>b"] = { name = "+buffer" },
+			["<leader>c"] = { name = "+comments" },
+			["<leader>f"] = { name = "+file/find" },
+			["<leader>g"] = { name = "+git" },
+			["<leader>q"] = { name = "+quit/session" },
+			["<leader>s"] = { name = "+search" },
+			["<leader>u"] = { name = "+ui" },
+			["<leader>w"] = { name = "+windows" },
+			["<leader>l"] = { name = "+diagnostics/quickfix" },
+		},
+	},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
+		wk.register(opts.defaults)
+	end,
+}
