84c233094f25a0f02c875222b2c1b927c8f34f50
Alexander Navarro <ale.navarro.parra@gmail.com>
2023-05-17 21:16:22 -0400

    add illuminate

--- config/nvim/lua/aleidk/plugins/init.lua
+++ config/nvim/lua/aleidk/plugins/init.lua
@@ -18,10 +18,10 @@ return {
 
 			-- Useful status updates for LSP
 			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-			{ "j-hui/fidget.nvim", opts = {} },
+			{ "j-hui/fidget.nvim",       opts = {} },
 
 			-- Additional lua configuration, makes nvim stuff amazing!
-			{ "folke/neodev.nvim", opts = {} },
+			{ "folke/neodev.nvim",       opts = {} },
 		},
 	},
 
@@ -55,5 +55,13 @@ return {
 				desc = "Close buffer",
 			},
 		},
+	},
+	{
+		"RRethy/vim-illuminate",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = { delay = 200 },
+		config = function(_, opts)
+			require("illuminate").configure(opts)
+		end,
 	},
 }
