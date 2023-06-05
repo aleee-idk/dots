23dc5489a1a35c8642af8ab71fb3ed1f48a58125
aleidk <ale.navarro.parra@gmail.com>
2023-06-04 22:45:14 -0400

    add starship promt

--- config/zsh/aliases/aliases.zsh
+++ config/zsh/aliases/aliases.zsh
@@ -5,32 +5,23 @@
 #fix obvious typo's
 alias pdw="pwd"
 
-#search content with ripgrep
-alias rg="rg --sort path"
-
-# move stuff arround
+# File Managment
 alias \
     cp='cp -iv' \
     mv='mv -iv' \
     cpr='rsync  -za --partial -hh --info=progress2' \
     mvr='rsync  -za --remove-source-files --partial -hh --info=progress2' \
     sync='rsync -zau --delete --partial -hh --info=progress2' \
-    md='mkdir -pv';
-
-# delete stuff
-alias \
     rm='trash -i' \
-    rmf='rm -rfI'
-
-# short long and common commands
-alias \
-    mkexec='chmod +x';
-
-# Exa for listing
-alias \
+    rmf='rm -rfI' \
+    md='mkdir -pv' \
     ls='exa -lh --color=always --icons --git ' \
     la='exa -alh --color=always --icons --git ' \
-    ld='exa -la --no-permissions --no-filesize --no-time --no-time --no-user'
+    ld='exa -la --no-permissions --no-filesize --no-time --no-time --no-user' \
+    mkexec='chmod +x';
+
+# Search and Query Files
+alias grep="rg --sort path --context 3";
 
 # Pacman
 alias \
@@ -41,10 +32,7 @@ alias \
     pkglast="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -25 | nl"
 
 
-alias \
-    reload='exec $SHELL -l' \
-
-    # Query Stuff
+# Query Stuff
 alias \
     ip='ip -human -color -brief addr' \
     df="duf --hide special --hide-mp '/boot,/boot/*'" \
@@ -62,6 +50,10 @@ alias \
     music-lofi-bg='ts lofi -d "mpv --no-video $lofi_url"' \
     music-ninten='mpv "$nintendo_url" &>/dev/null &!' \
     music-ninten-bg='ts lofi -d "mpv --no-video $nintendo_url"'
+
+# Misc
+alias \
+    reload='exec $SHELL -l' \
 
 #--------------------------------------------------------------------#
 #                               Global                               #
