a08df1abf1b59506690df74eedaa44d56c5bd8a1
aleidk <ale.navarro.parra@gmail.com>
2023-10-26 18:10:28 -0300

    change treesitter config

--- config/nvim/lua/aleidk/plugins/treesitter.lua
+++ config/nvim/lua/aleidk/plugins/treesitter.lua
@@ -7,86 +7,85 @@ return {
 		"JoosepAlviste/nvim-ts-context-commentstring",
 	},
 	build = ":TSUpdate",
-	opts = {
-		-- Add languages to be installed here that you want installed for treesitter
-		ensure_installed = {
-			"bash",
-			"c",
-			"cpp",
-			"go",
-			"lua",
-			"markdown",
-			"markdown_inline",
-			"python",
-			"regex",
-			"rust",
-			"tsx",
-			"typescript",
-			"vim",
-			"vimdoc",
-		},
-		-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-		auto_install = true,
-		highlight = { enable = true },
-		indent = { enable = true },
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<c-space>",
-				node_incremental = "<c-space>",
-				scope_incremental = "<c-s>",
-				node_decremental = "<M-space>",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			-- Add languages to be installed here that you want installed for treesitter
+			ensure_installed = {
+				"bash",
+				"c",
+				"cpp",
+				"go",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"regex",
+				"rust",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
 			},
-		},
-		textobjects = {
-			select = {
+			-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
 				enable = true,
-				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
 				keymaps = {
-					-- You can use the capture groups defined in textobjects.scm
-					["aa"] = "@parameter.outer",
-					["ia"] = "@parameter.inner",
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["ic"] = "@class.inner",
+					init_selection = "<c-space>",
+					node_incremental = "<c-space>",
+					scope_incremental = "<c-s>",
+					node_decremental = "<M-space>",
 				},
 			},
-			move = {
-				enable = true,
-				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next_start = {
-					["]m"] = "@function.outer",
-					["]]"] = "@class.outer",
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
 				},
-				goto_next_end = {
-					["]M"] = "@function.outer",
-					["]["] = "@class.outer",
+				move = {
+					enable = true,
+					set_jumps = true, -- whether to set jumps in the jumplist
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]]"] = "@class.outer",
+					},
+					goto_next_end = {
+						["]M"] = "@function.outer",
+						["]["] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[["] = "@class.outer",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+						["[]"] = "@class.outer",
+					},
 				},
-				goto_previous_start = {
-					["[m"] = "@function.outer",
-					["[["] = "@class.outer",
-				},
-				goto_previous_end = {
-					["[M"] = "@function.outer",
-					["[]"] = "@class.outer",
+				swap = {
+					enable = true,
+					swap_next = {
+						["<leader>a"] = "@parameter.inner",
+					},
+					swap_previous = {
+						["<leader>A"] = "@parameter.inner",
+					},
 				},
 			},
-			swap = {
-				enable = true,
-				swap_next = {
-					["<leader>a"] = "@parameter.inner",
-				},
-				swap_previous = {
-					["<leader>A"] = "@parameter.inner",
-				},
-			},
-		},
-		autotag = { enable = true },
-		context_commentstring = { enable = true, enable_autocmd = false },
-	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
+			autotag = { enable = true },
+			context_commentstring = { enable = true, enable_autocmd = false },
+		})
 
 		vim.opt.foldmethod = "expr"
 		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
