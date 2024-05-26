ff7073073c4c8df89347f595a749b25a50d9f7b2
aleidk <ale.navarro.parra@gmail.com>
2024-05-26 11:58:54 -0400

    Add octo.nvim for github issues management
    closes #1

--- chezmoi/dot_config/nvim/lua/aleidk/plugins/git.lua
+++ chezmoi/dot_config/nvim/lua/aleidk/plugins/git.lua
@@ -94,4 +94,21 @@ return {
 			},
 		},
 	},
+
+	{
+		"pwntester/octo.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = { enable_builtin = true },
+		keys = {
+			{
+				"<leader>go",
+				"<CMD>Octo<CR>",
+				desc = "Octo.nvim",
+			},
+		},
+	},
 }
