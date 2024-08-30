58a23e4a86e91ecf59a002084ceb24dc15f21d77
aleidk <ale.navarro.parra@gmail.com>
2024-08-30 09:13:40 -0400

    update bookmarks manager in nvim

--- chezmoi/dot_config/nvim/lua/aleidk/plugins/bookmarks.lua
+++ chezmoi/dot_config/nvim/lua/aleidk/plugins/bookmarks.lua
@@ -1,3 +1,13 @@
+local function select_or_create(search)
+	local grapple = require("grapple")
+
+	if grapple.exists(search) then
+		grapple.select(search)
+	else
+		grapple.tag()
+	end
+end
+
 return {
 	"cbochs/grapple.nvim",
 	dependencies = {
@@ -6,14 +16,19 @@ return {
 	lazy = false,
 	cmd = "Grapple",
 	keys = {
-		{ "<leader><leader>a", "<cmd>Grapple toggle<cr>", desc = "Toggle bookmark for current file" },
-		{ "<leader><leader>t", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle bookmarks window" },
-		{ "<leader><leader>T", "<cmd>Grapple toggle_scopes<cr>", desc = "Toggle scopes window" },
-		{ "<leader><leader>n", "<cmd>Grapple cycle forward<cr>", desc = "Next bookmark" },
-		{ "<leader><leader>N", "<cmd>Grapple cycle backward<cr>", desc = "Prev bookmark" },
-		{ "<leader><leader>j", "<cmd>Grapple select index=1<cr>", desc = "bookmark 1" },
-		{ "<leader><leader>k", "<cmd>Grapple select index=2<cr>", desc = "bookmark 2" },
-		{ "<leader><leader>l", "<cmd>Grapple select index=3<cr>", desc = "bookmark 3" },
-		{ "<leader><leader>ñ", "<cmd>Grapple select index=4<cr>", desc = "bookmark 4" },
+		{ "<leader><leader>a", "<cmd>Grapple toggle<cr>",                      desc = "Toggle bookmark for current file" },
+		{ "<leader><leader>D", "<cmd>Grapple reset<cr>",                       desc = "Delete all bookmarks" },
+		{ "<leader><leader>t", "<cmd>Grapple toggle_tags<cr>",                 desc = "Toggle bookmarks window" },
+		{ "<leader><leader>T", "<cmd>Grapple toggle_scopes<cr>",               desc = "Toggle scopes window" },
+		{ "<leader><leader>n", "<cmd>Grapple cycle forward<cr>",               desc = "Next bookmark" },
+		{ "<leader><leader>N", "<cmd>Grapple cycle backward<cr>",              desc = "Prev bookmark" },
+		{ "<leader><leader>j", function() select_or_create({ index = 1 }) end, desc = "Go or create bookmark 1" },
+		{ "<leader><leader>k", function() select_or_create({ index = 2 }) end, desc = "Go or create bookmark 2" },
+		{ "<leader><leader>l", function() select_or_create({ index = 3 }) end, desc = "Go or create bookmark 3" },
+		{ "<leader><leader>ñ", function() select_or_create({ index = 4 }) end, desc = "Go or create bookmark 4" },
+		{ "<leader><leader>J", "<cmd>Grapple tag index=1<cr>",                 desc = "Override bookmark 1" },
+		{ "<leader><leader>K", "<cmd>Grapple tag index=2<cr>",                 desc = "Override bookmark 2" },
+		{ "<leader><leader>L", "<cmd>Grapple tag index=3<cr>",                 desc = "Override bookmark 3" },
+		{ "<leader><leader>Ñ", "<cmd>Grapple tag index=4<cr>",                 desc = "Override bookmark 4" },
 	},
 }
